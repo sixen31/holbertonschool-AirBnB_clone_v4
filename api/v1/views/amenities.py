d08dd50542a829fd94431eed9bd1fe39e5880185
SpStigma <yoann.rivet0@gmail.com>
2023-12-21 11:01:44 +0100

    First attempt of 9. Amenity.

---
+++ api/v1/views/amenities.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python3
+"""Amenities"""
+from api.v1.views import app_views
+from flask import abort, jsonify, request
+from models import storage
+from models.amenity import Amenity
+
+
+@app_views.route('/amenities', methods=['GET'])
+def get_amenities():
+    """Retrieves the list of all Amenity objects"""
+    amenities = [
+        amenity.to_dict() for amenity in storage.all(Amenity).values()
+        ]
+    return jsonify(amenities)
+
+
+@app_views.route('/amenities/<amenity_id>', methods=["GET"])
+def get_amenity(amenity_id):
+    """Retrieves a Amenity object"""
+    amenity = storage.get(Amenity, amenity_id)
+    if amenity is None:
+        abort(404)
+    else:
+        return jsonify(amenity.to_dict())
+
+
+@app_views.route('/amenities/<amenity_id>', methods=['DELETE'])
+def delete_amenity(amenity_id):
+    """Deletes a Amenity object with id"""
+    amenity = storage.get(Amenity, amenity_id)
+    if amenity is None:
+        abort(404)
+    else:
+        storage.delete(amenity)
+        storage.save()
+
+    return jsonify({}), 200
+
+
+@app_views.route("/amenities", methods=["POST"])
+def post_amenity():
+    """Creates an Amenity"""
+    body = request.get_json()
+    if body is None:
+        abort(400, "Not a JSON")
+
+    if "name" not in body:
+        abort(400, "Missing name")
+
+    amenity = Amenity(**body)
+    storage.new(amenity)
+    storage.save()
+
+    return jsonify(amenity.to_dict()), 201
+
+
+@app_views.route("/amenities/<amenity_id>", methods=["PUT"])
+def update_amenity(amenity_id):
+    """Updates a Amenity object"""
+    amenity = storage.get(Amenity, amenity_id)
+    if amenity is None:
+        abort(404)
+    else:
+        request_http = request.get_json()
+        if request_http is None:
+            abort(400, 'Not a JSON')
+
+    for key, value in request_http.items():
+        if key not in ['id', 'created_at', 'updated_at']:
+            setattr(amenity, key, value)
+
+    storage.save()
+
+    return jsonify(amenity.to_dict()), 200
