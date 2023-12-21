e099a4bb0494944be310e9e39bfd0bd17e0b7a70
SpStigma <yoann.rivet0@gmail.com>
2023-12-21 01:20:32 +0100

    First attempt of error handle 404.

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Main module to start a Flask web application for the Airbnb clone project"""
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -14,6 +14,12 @@ app.register_blueprint(app_views)
 def teardown_appcontext(exception):
     """Tears down the application context, closing the storage connection"""
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found_error(error):
+    """Handle 404 errors and return a JSON response."""
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
